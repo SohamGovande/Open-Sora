e272ce8a37e094bf42eba1839d4581a6eb18e214
soham govande <vgsoham@gmail.com>
2024-11-07 15:13:58 -0800

    ensure rotary_embedding_torch patch is idempotent

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ sed -i 's/if use_kernel/if False/' opensora/models/layers/blocks.py
 sed -i 's/fmha.BlockDiagonalMask/fmha.attn_bias.BlockDiagonalMask/' opensora/models/layers/blocks.py
 
 ROTARY_FILE=$(python3 -c "import rotary_embedding_torch.rotary_embedding_torch as rotary_embedding_torch; print(rotary_embedding_torch.__file__)")
+sed -i "s/if False: self.register_buffer/self.register_buffer/" "$ROTARY_FILE"
 sed -i "s/self.register_buffer/if False: self.register_buffer/" "$ROTARY_FILE"
